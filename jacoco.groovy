976187a39785f7ef2bf5c1a9380df5d44a1c2006
László Urszuly <laszlo.urszuly@jayway.com>
2017-02-09 14:22:51 +0100

    Adds jacoco gradle script example

---
+++ jacoco.groovy
@@ -0,0 +1,21 @@
+apply plugin: 'jacoco'
+
+jacoco {
+    // At some point in time this was the only version working with
+    // Robolectric.
+    toolVersion = "0.7.6.201602180812"
+    reportsDir = file("${buildDir}/reports/coverage/")
+}
+
+android {
+    testOptions {
+        unitTests.all {
+            jacoco {
+                // Needed in order to have Robolectric play ball
+                // This command only works with Gradle 2.13+ though...
+                includeNoLocationClasses = true
+            }
+        }
+    }
+}
+
