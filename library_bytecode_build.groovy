15e51ee889fb085eca82089e00e6e614c6fb1bd9
László Urszuly <laszlourszuly@users.noreply.github.com>
2017-02-09 15:19:50 +0100

    Update library_bytecode_build.groovy

--- library_bytecode_build.groovy
+++ library_bytecode_build.groovy
@@ -1,7 +1,7 @@
 // Use the Android library gradle plugin
 apply plugin: 'com.android.library'
 
-// Force Retrolambda to output Java7 compatible byte code
+// Force Retrolambda output to Java7 bytecode version
 retrolambda {
     bytecodeVersion 51
 }
@@ -10,7 +10,7 @@ retrolambda {
 android {
     // ...
     
-    // Force AAR to correct bytecode version
+    // Force AAR output to Java7 bytecode version
     compileOptions {
         sourceCompatibility JavaVersion.VERSION_1_8
         targetCompatibility JavaVersion.VERSION_1_7
